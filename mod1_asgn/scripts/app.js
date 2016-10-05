c99c8b021c7f444fb69aac4b6d6e71ded15cdb96
Satish Thulva <sathya691@gmail.com>
2016-10-05 21:52:21 +0530

    committing changes forgot to commit

--- mod1_asgn/scripts/app.js
+++ mod1_asgn/scripts/app.js
@@ -8,7 +8,8 @@
    
     $scope.showTheVerdict = false;
     $scope.verdict = '';
-    $scope.lunch_items = '';
+    $scope.lunchItems = '';
+    $scope.styleClass = '';
 
     $scope.displayTheVerdict = function(){
       $scope.showTheVerdict = true;
@@ -16,18 +17,27 @@
 
     $scope.updateVerdict = function(){
       var numItems = 0;
-      var items = $scope.lunch_items.split(',');
+      var items = $scope.lunchItems.split(',');
       for(var index=items.length;index--;)
         if(items[index] !== '')
           numItems+=1;
 
-      $scope.verdict = numItems <= 3 ? 'Enjoy!' : 'Too much!';
+      if(numItems === 0)
+      {
+        $scope.styleClass = 'red';
+        $scope.verdict = 'Please enter data first';
+      }
+      else
+      {
+        $scope.styleClass = 'green';
+        $scope.verdict = numItems <= 3 ? 'Enjoy!' : 'Too much!';
+      }
     }
 
     $scope.reset = function(){
       $scope.verdict = '';
       $scope.displayTheVerdict = false;
-      $scope.lunch_items = '';
+      $scope.lunchItems = '';
     }
   }
 
