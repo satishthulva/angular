2a2bad4d116af965fbd5f2ebc5d6b39df3010942
Satish Thulva <sathya691@gmail.com>
2016-10-29 23:54:51 +0530

    mod3 asgn first cut

---
+++ mod3_asgn/scripts/app.js
@@ -0,0 +1,51 @@
+/**
+* Always make your javascript file IIFE - Immediately Invoked Function Expression - so that we won't mutate global scope unknowingly( because function creates its' own scope )
+**/
+(function(){
+angular.module('restaurantMenuSearchApp',[]).
+  controller('restaurantMenuSearchController', restaurantMenuSearchControllerFunc).
+  directive('matchedItems', restaurantMenuDirectiveFunc)
+
+// controller definition
+function restaurantMenuSearchControllerFunc($http){
+  var ref = this;
+  ref.matchedItems = [];
+
+  var temp;
+
+  /**
+  * Fetch the menu from restaurants' api and filter based on the search key
+  **/
+  ref.fetchMatchedItems = function(){
+    $http.get('').then(function(resp){
+        ref.matchedItems = [];
+        temp = resp.data;
+        for(var i=temp.length;i--;){
+          if(temp[i].description.indexOf(ref.searchKey) !== -1){
+            ref.matchedItems.push(temp[i]);
+          }
+        }
+    });
+  }
+}
+
+// Specifying dependencies
+restaurantMenuSearchControllerFunc.$inject = ['$http'];
+
+// directive Definitin Object returning function
+function restaurantMenuDirectiveFunc(){
+var ddo = {
+      restrict : 'E',
+      scope : {
+        matches : '=items',
+        removeItem = function(index){
+          matches.splice(index);
+        }
+      },
+      templateUrl : '../templates/restaurant_menu.html'
+  };
+
+  return ddo;
+}
+
+})();
